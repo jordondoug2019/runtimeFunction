6df340732b63c96166868ef54ea799a9a8cf6a1a
Ubuntu <ubuntu@ip-172-31-49-19.ec2.internal>
2024-09-05 22:24:01 +0000

    testing uptime calculations

---
+++ runtime.py
@@ -0,0 +1,15 @@
+#Write a Python function that calculates the uptime percentage of a service based on the total number of hours 
+#and the number of hours the service was down. The function should take 2 parameters(total hours and down hours, 
+#inputted when the function is run). 
+#Lastly, the function should return the uptime percentage rounded to two decimal places. 
+
+#take 2 numbers
+    #Uptime and Downtime
+#return uptime rounded to a percentage
+
+def runtime(tHours, dHours):
+    uptime= (tHours-dHours)/tHours-100
+    print(round(uptime,2))
+    return round(uptime,2)
+
+runtime(3600,500)
